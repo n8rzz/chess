f9e5052d087d6ed0c2baa3f1b9508f696d0e299e
Nate Geslin <teamtomkins23@gmail.com>
2018-09-02 13:58:39 -0500

    updates test configurations and tsconfigs to properly run all tests * updates debugger config

--- src/client/app.controller.ts
+++ src/client/app.controller.ts
@@ -1,16 +1,32 @@
 import StageViewController from './stage/stage-view.controller';
 
-class AppController {
+export default class AppController {
     private _stageViewController: StageViewController = null;
 
     constructor() {
-        const stageElement: SVGElement = document.getElementsByClassName('js-stage')[0] as SVGElement;
-        this._stageViewController = new StageViewController(stageElement);
+        return this._init()
+            ._createChildren();
     }
+
 
     public log(msg: string): void {
         console.log('+++', msg);
     }
+
+    private _init(): this {
+        return this;
+    }
+
+    private _createChildren(): this {
+        const stageElement: SVGElement = document.getElementsByClassName('js-stage')[0] as SVGElement;
+
+        if (typeof stageElement === 'undefined') {
+            return this;
+        }
+
+        this._stageViewController = new StageViewController(stageElement);
+
+        return this;
+    }
 }
 
-export default AppController;
